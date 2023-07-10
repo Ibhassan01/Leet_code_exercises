c077fe847dccc93d16e6750c6a84e09b2afc1c9a
Ibhassan01 <olorunnisolaibrahim46@gmail.com>
2023-07-10 21:49:39 +0100

    Done

---
+++ validParenthesis.js
@@ -0,0 +1,41 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+// determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+let isValid = (str) => {
+
+    let empty = [];
+
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] ==='(' || str[i] === '{' || str[i] === '['){
+            empty.push(str[i])
+        } else {
+
+            if(str.length === 0){
+                return false
+            }
+
+            let symbol = str[i];
+            let top = empty.pop(); 
+
+            if(
+                (symbol === ')' && top !== '(') ||
+                (symbol === '}' && top !== '{') ||
+                (symbol === ']' && top !== '[')
+            ){
+                return false
+            }
+            else{
+                return true;
+            }
+        }
+    }
+
+}
+
+console.log(isValid('{)'));
