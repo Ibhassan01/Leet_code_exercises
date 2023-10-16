4df4b20f30f86909d2f5b606e37848a42f487374
Ibhassan01 <olorunnisolaibrahim46@gmail.com>
2023-10-16 22:10:35 +0100

    Finds the most occuring elements in the array

---
+++ majorityElement.js
@@ -0,0 +1,27 @@
+// Given an array nums of size n, return the majority element.
+// The majority element is the element that appears more than ⌊n / 2⌋ times.
+//You may assume that the majority element always exists in the array.
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    let majority = null;
+    let count = 0;
+
+    for (const num of nums) {
+        if (count === 0) {
+            majority = num;
+        }
+
+        if(majority === num) {
+            count++;
+        } else {
+            count--
+        }
+    }
+    return majority
+};
+
+const nums = [1, 2, 3, 3, 3, 2, 4, 1, 5]
+console.log(majorityElement(nums));
