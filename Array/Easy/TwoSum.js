75fa600b9767faf86f066b698c128a4fbbfe515f
ibhassan01 <olorunnisolaibrahim46@gmail.com>
2025-09-04 20:16:26 +0100

    done

--- Array/Easy/TwoSum.js
+++ Array/Easy/TwoSum.js
@@ -9,7 +9,16 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  console.log('one')  
-};
-twoSum()
+const array = [1, 2, 3, 2, 4]
+const target = 4
+const twoSum = (nums, target) => {
+    for(i = 0; i < nums.length; i++){
+        for(j = i + 1; j < nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                return [i,j]
+            }
+        }
+    }
+}
+
+console.log(twoSum(array, target));
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 My Leet code solutions will be here
+
+Array
+ * Easy
+    1 Two Sum
