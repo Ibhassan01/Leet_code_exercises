39bfad5fae74048656b87210023fd3cc0438e18a
ibhassan01 <olorunnisolaibrahim46@gmail.com>
2025-11-11 00:05:28 +0100

    removeDuplicates.js

--- Array/Easy/longestCommonPrefix.js
+++ Array/Easy/longestCommonPrefix.js
@@ -6,15 +6,15 @@
  * @return {string}
  */
 const longestCommonPrefix = (strs) => {
-    if(!strs.length) return "";
+    if (!strs.length) return "";
     let prefix = strs[0];
-    for(let i = 1; i < strs.length; i++){
+    for (let i = 1; i < strs.length; i++) {
         let j = 0;
-        while(j< prefix.length && j < strs[i].length && prefix[j] ===strs[i][j]){
+        while (j < prefix.length && j < strs[i].length && prefix[j] === strs[i][j]) {
             j++
         }
         prefix = prefix.slice(0, j);
-        if(prefix === "") return "";
+        if (prefix === "") return "";
     }
     return prefix;
 }
